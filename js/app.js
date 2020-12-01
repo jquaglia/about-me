8a4200feb1b05815acc642475f44f37fe41d8e04
stubtail42 <jqugali@gmail.com>
2020-12-01 14:52:46 -0800

    Added in bio to index, some small styling to it and added a question in js

--- js/app.js
+++ js/app.js
@@ -1,29 +1,35 @@
 'use strict';
 
 var userName = prompt('What is your name?');
-// string concatenation
-console.log('The user name is ' + userName + '!');
+// console.log('The user name is ' + userName + '!');
 
 alert('Hello ' + userName + '!');
 
-// question one
+var codeName = prompt('Is the maker of this website\'s name Jason?').toLowerCase();
+// console.log(codeName);
+
+if(codeName === 'yes' || codeName === 'y'){
+  alert('Ding! Ding! Ding! We have a winner!');
+}
+
+// question two
 // DONE - ask question y/n -normalize case
-var homeTown = prompt('Do i live in Seattle?').toLowerCase();
+var homeTown = prompt('Do I live in Seattle?').toLowerCase();
 
 // console.log(homeTown);
 
 if(homeTown === 'yes' || homeTown === 'y'){
   // DONE - console.log the answer IF IT IS CORRECT in a string concatenation
-  // TODO - if it works, comment out console.log, send alert to user
+  // DONE - if it works, comment out console.log, send alert to user
   // console.log('Yes I live in Seattle.' + userName + ' answered ' + homeTown);
   alert('That\'s Correct!');
 } else{
-  alert('you\'re wrong!');
+  alert('You\'re wrong!');
 }
 
 // question two
-// TODO - ask question
-// TODO - console.log the answer in a string concatenation
+// TODO - ask question with yes or no answer, normalize case
+// TODO - console.log the answer in a string concatenation IF IT IS CORRECT
 // TODO - if it works, comment out console.log, send alert to user
 
 // TODO - change alert: thank user, give meaning message with their name
