aca42ba5342c4f58a167c502c3299eb85b009ae3
stubtail42 <jqugali@gmail.com>
2020-12-01 12:32:04 -0800

    added readme template

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# about-me
+# LAB - 2
+
+## About Me
+
+I built an app with a direct and powerful purpose. My app does all of the things that it accomplishes well. Here  I describe the purpose and functionality so those that visit my README understand the app.
+
+### Author: Jason Quaglia/Code 201d70
+
+### Links and Resources
+
+* [Relevant toLowerCase info](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase)
+
+* Any Links you used as reference
+
+### Reflections and Comments
+
+* Consider including the answers to your daily journal and submission questions here
+
+* This is also a good place to reflect on the tools and resources used and learned.
+
+### Collaborators
+
+* the people at your table
--- js/app.js
+++ js/app.js
@@ -6,17 +6,45 @@ console.log('The user name is ' + userName + '!');
 
 alert('Hello ' + userName + '!');
 
-var iceCream = prompt('What is your favorite flavour of ice cream?');
-console.log(userName + ' likes ' + iceCream + ' ice cream!');
+// question one
+// DONE - ask question y/n -normalize case
+var homeTown = prompt('Do i live in Seattle?').toLowerCase();
 
-alert('Hello ' + userName + ', who loves ' + iceCream + ' ice cream!');
+// console.log(homeTown);
 
-var topping = prompt('What is your favorite topping?');
-console.log(userName + ' likes ' + iceCream + ' ice cream with ' + topping + '!');
+if(homeTown === 'yes' || homeTown === 'y'){
+  // DONE - console.log the answer IF IT IS CORRECT in a string concatenation
+  // TODO - if it works, comment out console.log, send alert to user
+  // console.log('Yes I live in Seattle.' + userName + ' answered ' + homeTown);
+  alert('That\'s Correct!');
+} else{
+  alert('you\'re wrong!');
+}
 
-alert('Hello ' + userName + ', who loves ' + iceCream + ' ice cream with ' + topping + '!');
+// question two
+// TODO - ask question
+// TODO - console.log the answer in a string concatenation
+// TODO - if it works, comment out console.log, send alert to user
 
-var visit = prompt('Where have you always wanted to travel to?');
-console.log(userName + ' likes ' + iceCream + ' ice cream with ' + topping + ' when in ' + visit + '!');
+// TODO - change alert: thank user, give meaning message with their name
 
-alert('Hello ' + userName + ', who has always wanted to eat ' + iceCream + ' ice cream with ' + topping + ' in an ice cream shop in ' + visit + '!');
+
+
+
+
+
+
+// var iceCream = prompt('What is your favorite flavour of ice cream?');
+// console.log(userName + ' likes ' + iceCream + ' ice cream!');
+
+// alert('Hello ' + userName + ', who loves ' + iceCream + ' ice cream!');
+
+// var topping = prompt('What is your favorite topping?');
+// console.log(userName + ' likes ' + iceCream + ' ice cream with ' + topping + '!');
+
+// alert('Hello ' + userName + ', who loves ' + iceCream + ' ice cream with ' + topping + '!');
+
+// var visit = prompt('Where have you always wanted to travel to?');
+// console.log(userName + ' likes ' + iceCream + ' ice cream with ' + topping + ' when in ' + visit + '!');
+
+// alert('Hello ' + userName + ', who has always wanted to eat ' + iceCream + ' ice cream with ' + topping + ' in an ice cream shop in ' + visit + '!');
